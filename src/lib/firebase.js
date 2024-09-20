f42e20ce7634854184d6c365e378f7e5ac5414cb
Harsh Patel <65148342+HarshPatel91298@users.noreply.github.com>
2024-09-20 12:00:07 -0400

    Issue Fixed .env
    Next Js required NEXT_PIBLUC prefix in env.
    
    Added this prefix in .env file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,15 +8,19 @@ require('dotenv').config();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PRODUCT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PRODUCT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+console.log("Firebase Config: ", firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
